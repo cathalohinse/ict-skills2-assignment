a8800901ac98cb916c4104f2569d44ccac323f1b
Cathal Henchy <20091405@mail.wit.ie>
2021-06-13 10:11:44 +0100

    Change site header

--- src/index.js
+++ src/index.js
@@ -5,19 +5,13 @@ import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage"; // NEW
 import MovieReviewPage from "./pages/movieReviewPage";
+import SiteHeader from './components/siteHeader';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/movies/favorites">Favorites</Link>
-        </li>
-      </ul>
-      <Switch>
+      <BrowserRouter>
+          <SiteHeader />      {/* New Header  */}
+          <Switch>
         <Route path="/reviews/:id" component={MovieReviewPage} />
         <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
         <Route path="/movies/:id" component={MoviePage} />
